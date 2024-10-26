5f2dc6667a66ac77e3c1cd2f0e15702cb96dd0ed
Nikita Mashchenko <mashchenko1@icloud.com>
2024-10-25 22:44:38 -0500

    hotfix: disable all scores

--- src/app/routes/app/data.tsx
+++ src/app/routes/app/data.tsx
@@ -10,7 +10,7 @@ import { getBiomarkersQueryOptions } from '@/features/biomarkers/api';
 import { BiologicalAgeCard } from '@/features/biomarkers/components/biological-age-card';
 import { BiomarkersDataTable } from '@/features/biomarkers/components/biomarkers-data-table/biomarker-data-table';
 import { BiomarkersSummaryCard } from '@/features/biomarkers/components/biomarkers-summary-card';
-import { ScoreCard } from '@/features/biomarkers/components/score-card';
+// import { ScoreCard } from '@/features/biomarkers/components/score-card';
 
 export const dataLoader = (queryClient: QueryClient) => async () => {
   /**
@@ -51,7 +51,7 @@ export const DataRoute = () => {
         id="summary"
         className="flex flex-col gap-5 pt-6 xl:flex-row xl:pb-16"
       >
-        <ScoreCard />
+        {/*<ScoreCard />*/}
         <BiologicalAgeCard />
         <BiomarkersSummaryCard />
       </section>
