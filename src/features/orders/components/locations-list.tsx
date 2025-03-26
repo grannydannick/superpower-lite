604aef31e32cbe03f02555702639aabe26b9a3c7
audric <108915409+aserador@users.noreply.github.com>
2025-03-26 02:07:54 -0700

    feat: increase height of address selector & add scrollbar (#297)
    * feat: increase height of address selector
    
    * feat: scrollbar on lab address selector

--- src/features/orders/components/locations-list.tsx
+++ src/features/orders/components/locations-list.tsx
@@ -24,45 +24,48 @@ export const LocationList = ({
   }
 
   return (
-    <div className="max-h-[240px] overflow-y-scroll rounded-2xl border border-zinc-200 bg-white p-2">
-      <RadioGroup
-        className="flex flex-col"
-        defaultValue={formatAddress(location?.address)}
-      >
-        {locations?.map((option, index) => (
-          <Label
-            key={index}
-            className={cn(
-              'rounded-lg py-4 px-6 text-left transition-all hover:bg-accent flex cursor-pointer items-center gap-4',
-              formatAddress(location?.address) === formatAddress(option.address)
-                ? 'bg-accent'
-                : null,
-            )}
-            onClick={() => {
-              updateLocation(option);
-            }}
-            htmlFor={`item-${index}`}
-          >
-            <RadioGroupItem
-              value={formatAddress(option.address)}
-              id={`item-${index}`}
-            />
-            <div className="flex flex-col items-start gap-1">
-              <Body1 className="text-zinc-600">
-                {formatAddress(option.address)}
-              </Body1>
-              <div className="flex flex-row items-center gap-px">
-                <MapPin className="h-4 min-w-4 text-zinc-400" />
-                <Body3 className="text-zinc-400">
-                  {option.name
-                    ? `${option.name} ( ${option.distance} mile${option.distance > 1 ? 's' : ''} )`
-                    : `${option.distance} mile${option.distance > 1 ? 's' : ''}`}
-                </Body3>
+    <div className="overflow-hidden rounded-2xl border border-zinc-200 bg-white p-1">
+      <div className="max-h-[320px] overflow-y-auto scrollbar scrollbar-thumb-zinc-300 [overflow:overlay] [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar]:w-2">
+        <RadioGroup
+          className="flex flex-col"
+          defaultValue={formatAddress(location?.address)}
+        >
+          {locations?.map((option, index) => (
+            <Label
+              key={index}
+              className={cn(
+                'rounded-lg py-4 px-6 text-left transition-all hover:bg-accent flex cursor-pointer items-center gap-4',
+                formatAddress(location?.address) ===
+                  formatAddress(option.address)
+                  ? 'bg-accent'
+                  : null,
+              )}
+              onClick={() => {
+                updateLocation(option);
+              }}
+              htmlFor={`item-${index}`}
+            >
+              <RadioGroupItem
+                value={formatAddress(option.address)}
+                id={`item-${index}`}
+              />
+              <div className="flex flex-col items-start gap-1">
+                <Body1 className="text-zinc-600">
+                  {formatAddress(option.address)}
+                </Body1>
+                <div className="flex flex-row items-center gap-px">
+                  <MapPin className="h-4 min-w-4 text-zinc-400" />
+                  <Body3 className="text-zinc-400">
+                    {option.name
+                      ? `${option.name} ( ${option.distance} mile${option.distance > 1 ? 's' : ''} )`
+                      : `${option.distance} mile${option.distance > 1 ? 's' : ''}`}
+                  </Body3>
+                </div>
               </div>
-            </div>
-          </Label>
-        ))}
-      </RadioGroup>
+            </Label>
+          ))}
+        </RadioGroup>
+      </div>
     </div>
   );
 };
