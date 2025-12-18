0474f64bdd43213e486649f49bb04837768047ce
Florian Kiem <92092993+flornkm@users.noreply.github.com>
2025-12-18 10:56:01 +0100

    fix: check if last protocol or not for routing (#1144)

--- src/features/protocol/components/goals/protocol-goal-view.tsx
+++ src/features/protocol/components/goals/protocol-goal-view.tsx
@@ -3,7 +3,7 @@ import { Link } from '@/components/ui/link';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Body1, Body2 } from '@/components/ui/typography';
 
-import { useProtocol } from '../../api';
+import { useLatestProtocol, useProtocol } from '../../api';
 import { ProtocolLayout } from '../layouts/protocol-layout';
 
 import { ProtocolGoal } from './protocol-goal';
@@ -21,9 +21,16 @@ export function ProtocolGoalView({
   protocolId,
   goalId,
 }: ProtocolGoalViewProps) {
+  const { data: lastProtocol, isLoading: isLatestProtocolLoading } =
+    useLatestProtocol();
   const { data: protocol, isLoading, error } = useProtocol(protocolId);
 
-  if (isLoading) {
+  const protocolLink =
+    lastProtocol?.id === protocolId
+      ? '/protocol'
+      : `/protocol/plans/${protocolId}`;
+
+  if (isLoading || isLatestProtocolLoading) {
     return (
       <div className="mx-auto w-full max-w-2xl space-y-6 p-6 lg:px-0">
         <Skeleton className="h-56 w-full rounded-2xl" />
@@ -66,7 +73,7 @@ export function ProtocolGoalView({
     <ProtocolLayout className="lg:pt-7">
       <div className="sticky top-20 hidden w-40 shrink-0 lg:block">
         <Link
-          to="/protocol"
+          to={protocolLink}
           className="group -ml-1.5 flex items-center gap-0.5 p-0"
         >
           <ChevronLeft className="-mt-px w-[15px] text-zinc-400 transition-all duration-150 group-hover:-translate-x-0.5 group-hover:text-zinc-600" />
